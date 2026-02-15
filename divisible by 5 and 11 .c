56628ab10830669b781b5592d57d17fcdbf39a5b
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-15 16:34:06 +0530

    Create divisible by 5 and 11 .c

---
+++ divisible by 5 and 11 .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 5 == 0 && num % 11 == 0){
+        printf("%d is divisible by both 5 and 11\n", num);
+    }else{
+        printf("%d is NOT divisible by both 5 and 11", num);
+    }
+    return 0;
+}
