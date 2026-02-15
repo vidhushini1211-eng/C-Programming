2a0bc5176991f35623bf4da61f1e366404a5db1f
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-15 16:07:04 +0530

    Create Even or odd .c

---
+++ Even or odd .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+    if (num % 2 == 0)
+        printf("%d is Even\n", num);
+    else
+        printf("%d is Odd\n", num);
+    return 0;
+}
