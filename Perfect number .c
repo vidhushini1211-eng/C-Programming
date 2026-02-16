989943bf5307b00b2cd48603e5e0654cd480f902
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-16 21:42:01 +0530

    Create Perfect number .c

---
+++ Perfect number .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(int i = 1; i <= num/2; i++) {
+        if(num % i == 0)
+            sum += i;
+    }
+    if(sum == num)
+        printf("%d is a Perfect Number", num);
+    else
+        printf("%d is NOT a Perfect Number", num);
+    return 0;
+}
