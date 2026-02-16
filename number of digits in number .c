59061f7e6433c15798f58c80477958f849aa4e98
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-16 20:31:19 +0530

    Create number of digits in number .c

---
+++ number of digits in number .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int num, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num == 0)
+        count = 1;
+    else {
+        while(num != 0) {
+            num /= 10;
+            count++;
+        }
+    }
+    printf("Number of digits = %d", count);
+    return 0;
+}
