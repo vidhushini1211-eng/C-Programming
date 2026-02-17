fe128b14fd446b6504aabb875a882514beb4f0e3
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-17 19:47:51 +0530

    Create LCM of 2 number .c

---
+++ LCM of 2 number .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b, i, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    for (i = (a > b ? a : b); ; i++) {
+        if (i % a == 0 && i % b == 0) {
+            lcm = i;
+            break;
+        }
+    }
+    printf("LCM = %d", lcm);
+    return 0;
+}
