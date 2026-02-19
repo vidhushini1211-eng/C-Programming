26196e32202a57c0233966acf37ae8d8bc286c9a
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-02-19 22:10:50 +0530

    Create Add 2 matrix .c

---
+++ Add 2 matrix .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int r,c,i,j,a[10][10],b[10][10];
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)
+        scanf("%d",&a[i][j]);
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)
+        scanf("%d",&b[i][j]);
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)
+        printf("%d ",a[i][j]+b[i][j]);
+        printf("\n");
+    }
+}
