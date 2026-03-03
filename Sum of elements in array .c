7d4b3287c7d6267a8fa28f681e9c0166903d617e
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-03-03 20:28:05 +0530

    Create Sum of elements in array .c

---
+++ Sum of elements in array .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a[50], n, i, sum = 0;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
