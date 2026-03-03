5f3917eab7655841a40d2febec1c87d1224fdc6d
vidhushini1211-eng <vidhushini1211@gmail.com>
2026-03-03 20:40:43 +0530

    Create delete element in array .c

---
+++ delete element in array .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a[50], n, pos, i;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &pos);
+    for(i = pos-1; i < n-1; i++)
+        a[i] = a[i+1];
+    for(i = 0; i < n-1; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
